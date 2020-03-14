5d59d98a89f3e6b4b2c413da50d06f7079d4123c
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2020-03-14 00:00:05 -0300

    TP 1 Lab 4

---
+++ src/main/java/com/utn/tsp/Tp01Lab4Application.java
@@ -0,0 +1,13 @@
+package com.utn.tsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp01Lab4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp01Lab4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/utn/tsp/Tp01Lab4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utn.tsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp01Lab4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
